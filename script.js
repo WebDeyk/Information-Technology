4eb0876933e50c85715752d59b48d9cde4e1223f
WebDeyk <webdeyk@gmail.com>
2025-03-13 17:16:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
